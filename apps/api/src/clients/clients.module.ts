d0736d1333528c8cd21e0c4a41f72f14816fda71
eportepro <eportepro@gmail.com>
2026-05-21 15:24:24 +0200

    fix(clients): importar AuthModule para resolver JwtAuthGuard

--- apps/api/src/clients/clients.module.ts
+++ apps/api/src/clients/clients.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [ClientsController],
   providers: [ClientsService],
   exports: [ClientsService],
